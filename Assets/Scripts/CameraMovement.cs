79f53449ac5cf55e3c072c307a86a64a098ded00
gsanz95 <gsanz95@gmail.com>
2018-12-09 20:13:56 -0600

    Camera and Generic Movement Scripts

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraMovement : MonoBehaviour {
+
+	public Transform followingTarget;
+	public float deltaX;
+	public float deltaY;
+	public float deltaZ;
+	public float lerpSpeed;
+
+	private Transform camera;
+	
+	//
+	void Start()
+	{
+		this.camera = gameObject.GetComponent<Transform>();
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		updateMovement();
+	}
+
+	void updateMovement(){
+		Vector3 desiredPosition = new Vector3(followingTarget.position.x + deltaX,
+											followingTarget.position.y + deltaY,
+											followingTarget.position.z + deltaZ);
+
+		camera.position = Vector3.Lerp(camera.position, desiredPosition, lerpSpeed);
+	}
+}
---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour {
+
+	public float maxYaw;
+	public float maxRoll;
+	public float lerpSpeed;
+
+	private Rigidbody planeBody;
+	private Vector3 deltaEulerAngle;
+	private Quaternion startingRotation;
+
+	// Use this for initialization
+	void Start () {
+		this.planeBody = gameObject.GetComponent<Rigidbody>();
+		this.startingRotation = this.planeBody.rotation;
+	}
+
+	void FixedUpdate()
+	{
+		deltaEulerAngle = new Vector3(-getYaw(), 0, -getRoll());
+		Quaternion deltaRotation = Quaternion.Euler(deltaEulerAngle) * this.startingRotation;
+		deltaRotation = Quaternion.Lerp(this.planeBody.rotation, deltaRotation, lerpSpeed);
+		planeBody.MoveRotation(deltaRotation);
+	}
+
+	float getRoll()
+	{
+		float delta = Input.GetAxisRaw("Vertical");
+		Debug.Log("Roll:" + delta);
+		return delta * this.maxRoll;
+	}
+
+	float getYaw()
+	{
+		float delta = Input.GetAxisRaw("Horizontal");
+		Debug.Log("Yaw" + delta);
+
+		return delta * this.maxYaw;
+	}
+}
